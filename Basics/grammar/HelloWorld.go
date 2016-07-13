09ba53cb41c53cc3bb40116a3558cedfa0a294f5
bullub <123572970@qq.com>
2016-07-13 13:07:12 +0800

    初始化提交,第一天学习go  hello world

--- Basics/grammar/HelloWorld.go
+++ Basics/grammar/HelloWorld.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+
+	for i := 0; i < 10; i ++ {
+		fmt.Printf("%d", i);
+	}
+
+	fmt.Println();
+	var i int;
+
+	i = 0;
+
+	for {
+		if i > 10 {
+			break;
+		}
+		i ++;
+		fmt.Println(i);
+	}
+
+	var iArr [3]int;
+
+	iArr = [3]int{
+		'a', 'b', 'c',
+	}
+
+	for k, v := range iArr {
+		fmt.Println(k, "=", v)
+	}
+
+	j := 0;
+
+	L:
+
+	if j < 10 {
+		fmt.Println("j=", j);
+		j ++;
+		goto L;
+	}
+
 	fmt.Printf("%d\n", 10)
 }
